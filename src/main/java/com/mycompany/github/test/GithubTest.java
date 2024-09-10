0d875a17e129467611e894bea6a1fffe092a4316
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-10 12:35:35 +0000

    First commit

---
+++ src/main/java/com/mycompany/github/test/GithubTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github.test;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class GithubTest {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int sum=a+b;
+    }
+    
+}
